9e5f27afa5dac54c699b5fa112bfe36fbbd387f1
Chris Gnam <crgnam@buffalo.edu>
2022-01-15 12:21:46 -0700

    updated tests

--- tests/test_orbits.py
+++ tests/test_orbits.py
@@ -1,11 +1,18 @@
 from ceres.orbits import TwoBody
-from ceres.constants import muSun
+from ceres.constants import muSun,AU,SPD
+import numpy as np
 
 def test_twobody():
-    a = 1
-    e = 1
-    i = 1
-    w = 1
-    RAAN = 1
-    ma = 1
-    orbit = TwoBody(muSun,a=a,e=e,i=i,w=w,RAAN=RAAN,ma=ma)
+    # Orbital elements for CERES (from SBDB):
+    a = 2.766043062222408*AU
+    e = 0.07850100198908602
+    i = np.deg2rad(10.58769305845201)
+    peri = np.deg2rad(73.63703979153577)
+    RAAN = np.deg2rad(80.26859547732911)
+    M = np.deg2rad(291.3755993017663)
+    epoch = 2459600.5*SPD
+
+    # Create the orbit instance:
+    orbit = TwoBody(muSun,'elements',np.array([a,e,i,peri,RAAN,M]),epoch)
+
+test_twobody()
